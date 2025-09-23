c6ec0c7e9bfcec204f4ae8162195fb490184a154
Tejas Dhabadkar <76923341+iam-tejas79@users.noreply.github.com>
2025-09-23 20:49:50 +0530

    Time: 3 ms (80.07%) | Memory: 46.6 MB (6.08%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        if (n == 0) {
+            return 0;
+        }
+
+        int[] candies = new int[n];
+        Arrays.fill(candies, 1);
+
+        // First pass: left to right
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        // Second pass: right to left
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+
+        // Sum up the candies
+        int totalCandies = 0;
+        for (int candy : candies) {
+            totalCandies += candy;
+        }
+
+        return totalCandies;
+    }
+}

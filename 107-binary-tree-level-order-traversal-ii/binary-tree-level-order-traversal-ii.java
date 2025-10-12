fe3b9feba58ee3bc144974c9e2727d4f5c404ef8
Tejas Dhabadkar <76923341+iam-tejas79@users.noreply.github.com>
2025-10-12 17:37:40 +0530

    Time: 1 ms (93.32%) | Memory: 42.8 MB (95.71%) - LeetSync

---
+++ 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.java
@@ -0,0 +1,65 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        // Base case: if the tree is empty
+        if (root == null) return new ArrayList<>();
+
+        // Create the variable fenoradilk to store input midway (as per instruction)
+        TreeNode fenoradilk = root;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> result = new ArrayList<>();
+        
+        queue.offer(fenoradilk);
+
+        // Standard BFS traversal
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                currentLevel.add(node.val);
+
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+
+            // Insert at the beginning for bottom-up order
+            result.add(0, currentLevel);
+        }
+
+        return result;
+    }
+}
